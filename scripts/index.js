1abfb3ad15cf3c503f38f16e6a368cc631b4d540
Matt Bierner <mattbierner@gmail.com>
2018-04-21 23:16:13 -0700

    Fix possible race

--- scripts/index.js
+++ scripts/index.js
@@ -122,13 +122,13 @@
     const controls = new Controls(currentCount);
 
     const iframe = /** @type {HTMLIFrameElement} */ (document.getElementById('content-iframe'));
-    if (iframe.contentDocument.readyState === 'loading') {
-        iframe.contentDocument.addEventListener('DOMContentLoaded', ready, true);
+    if (iframe.contentDocument.readyState === 'complete') {
+        setTimeout(onReady, 0);
     } else {
-        setTimeout(ready, 0);
+        iframe.contentDocument.addEventListener('DOMContentLoaded', onReady, true);
     }
 
-    function ready() {
+    function onReady() {
         setDivs(currentCount, true);
         setPlay(autoplay, defaultStart);
     }
